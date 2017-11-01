ca9a3eb5f701f6264ff52045bee0d70bdf9eed5f
Koitharu <unravel22@waifu.club>
2017-11-01 08:09:10 +0200

    Database and auth

---
+++ models.py
@@ -0,0 +1,50 @@
+from sqlalchemy import func
+
+from web import db
+
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+	login = db.Column(db.String(24), index=True, unique=True, nullable=False)
+	password = db.Column(db.String(32), nullable=False)
+	name = db.Column(db.String(46), nullable=False)
+	created_at = db.Column(db.TIMESTAMP, nullable=False, default=func.current_timestamp())
+
+
+class Category(db.Model):
+	__tablename__ = 'categories'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+	name = db.Column(db.String(24), unique=True, nullable=False)
+	created_at = db.Column(db.TIMESTAMP, nullable=False, default=func.current_timestamp())
+
+
+class Note(db.Model):
+	__tablename__ = 'notes'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+	category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
+	title = db.Column(db.String(42), nullable=False)
+	content= db.Column(db.Text, nullable=False)
+	created_at = db.Column(db.TIMESTAMP, nullable=False, default=func.current_timestamp())
+	updated_at = db.Column(db.TIMESTAMP, nullable=False, default=func.current_timestamp())
+	state = db.Column(db.Enum('default', 'deleted'), default='default', nullable=False)
+
+
+class ToDoList(db.Model):
+	__tablename__ = 'todo_lists'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+	category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), primary_key=True, nullable=False)
+	title = db.Column(db.String(42), nullable=False)
+	target_date = db.Column(db.Date, nullable=False)
+	created_at = db.Column(db.TIMESTAMP, nullable=False, default=func.current_timestamp())
+	state = db.Column(db.Enum('default', 'deleted'), default='default', nullable=False)
+
+
+class ToDoItem(db.Model):
+	__tablename__ = 'todo_items'
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+	list_id = db.Column(db.Integer, db.ForeignKey('todo_lists.id'), nullable=False)
+	content = db.Column(db.String(84), nullable=False)
+	checked = db.Column(db.Boolean, default=False, nullable=False)
+	created_at = db.Column(db.TIMESTAMP, nullable=False, default=func.current_timestamp())
--- web.py
+++ web.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template
+import hashlib
+from flask import Flask, render_template, request, jsonify, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
+from models import User
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.secret_key = 'rwf0r3rj0e0rjnenr003dc'
 db = SQLAlchemy(app)
+
+
+@app.route('/ajax/auth', methods=['POST'])
+def auth():
+	user = User.query() \
+		.filter_by(User.login == request.form['login']) \
+		.filter_by(User.password == hashlib.md5(request.form['password'])) \
+		.one_or_none()
+	if user is not None:
+		session['user'] = user.id
+		return jsonify({'result': True})
+	else:
+		return jsonify({'result': False})
+
+
+@app.route('/logout')
+def logout():
+	session.pop('user', None)
+	return redirect('/')
 
 
 @app.route('/')
